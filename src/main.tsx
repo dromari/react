32a6e31912447759917f4744f8e2762cfc82f865
Dromari <7709722@gmail.com>
2026-05-18 22:43:27 +0300

    fix: switch to hash router for github pages deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,43 +1,38 @@
 /// <reference types="vite/client" />
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import App from './App.tsx';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 import About from './components/About/About';
 import NotFound from './components/NotFound/NotFound';
 import PokemonDetails from './components/PokemonDetails/PokemonDetails';
 
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: (
-        <ErrorBoundary>
-          <App />
-        </ErrorBoundary>
-      ),
-      errorElement: <NotFound />,
-      children: [
-        {
-          path: 'pokemon/:detailsId',
-          element: <PokemonDetails />,
-        },
-      ],
-    },
-    {
-      path: '/about',
-      element: <About />,
-    },
-    {
-      path: '*',
-      element: <NotFound />,
-    },
-  ],
+const router = createHashRouter([
   {
-    basename: import.meta.env.BASE_URL,
-  }
-);
+    path: '/',
+    element: (
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    ),
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: 'pokemon/:detailsId',
+        element: <PokemonDetails />,
+      },
+    ],
+  },
+  {
+    path: '/about',
+    element: <About />,
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+]);
 
 const rootElement = document.getElementById('root');
 
